77d349823cc22fc8d2be404844c889bda524f187
samanthmm <90998580+samanthmm@users.noreply.github.com>
2023-07-16 14:47:29 +0530

    added register user

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [name,setname] = useState('');
+  function registerUser(){
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Register</h1>
+      <form onSubmit ={registerUser}>
+        <input type='text' placeholder='Name'
+        value = {name} onChange={(e)=>setname(e.target.value)}></input>
+        <input type= 'submit' value='Register'/>
+      </form>
     </div>
   );
 }
